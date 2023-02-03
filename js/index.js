01ce222e6b166f324ac153f484cd24f3b561ccb6
Iskander Nureev <iskander.qq@mail.ru>
2023-02-03 14:57:40 +0300

    Added delete function

--- js/index.js
+++ js/index.js
@@ -11,9 +11,62 @@ addButton.addEventListener('click', ev =>{
     popupWrapper.classList.toggle("hidden");
     
 });
-delButton.addEventListener('click', ev =>{
 
+delButton.addEventListener('click', ev =>{
+    let checkBtn = document.querySelectorAll(".checkBox-button");
+    let check = document.querySelectorAll(".checkBox-button__checked");
+    if(delButton.dataset.isPushed == 0){
+        delButton.dataset.isPushed = 1;
+        
+        checkBtn.forEach(el =>{
+            el.style.display = "flex";
+            
+        })
+        document.querySelectorAll(".buttons-img__button").forEach(el =>{
+            el.style.display = "none";
+            
+        })
+        
+        for(let i = 0; i< checkBtn.length; i++){
+            checkBtn[i].onclick = ()=>{
+                check[i].classList.toggle("hidden");
+                if(check[i].classList.contains("hidden")){
+                    check[i].dataset.ischecked = 0;
+                }else{
+                    check[i].dataset.ischecked = 1;
+                }
+            }
+        }    
+    }
+    
+    
+    delButton.onclick = ()=>{
+            check.forEach(el =>{
+                console.log(el )
+                if (el.dataset.ischecked == 1) {
+                    let par = el.parentElement
+                    for(let i = 0; i < 2; i++){
+                        par = par.parentElement;
+                    }
+                    par.remove()
+                }
+            })
+
+            delButton.dataset.isPushed = 0;
+            checkBtn.forEach(el =>{
+                el.style.display = "none";
+                
+            })
+            document.querySelectorAll(".buttons-img__button").forEach(el =>{
+                el.style.display = "block";
+                
+            })
+            delButton.onclick= null;
+    }
+    
 });
+
+
 popCloseButton.addEventListener('click', ev=>{
     if(ev.target.className === "popup-container__close__button"){
         popupWrapper.classList.toggle("hidden");
@@ -65,10 +118,24 @@ function createTask(){
     let taskButtonImg2 = document.createElement("img");
     taskButtonImg2.setAttribute("src", "img/edit.png");
 
+
+
+    let checkBox = document.createElement("div");
+    checkBox.classList.add("checkBox-button");
+
+    let checkBoxChecked = document.createElement("div");
+    checkBoxChecked.classList.add("checkBox-button__checked");
+    checkBoxChecked.classList.add("hidden");
+
+    checkBox.append(checkBoxChecked);
+
+
+
     taskButton1.append(taskButtonImg1);
     taskButton2.append(taskButtonImg2);
     taskButtonsDiv.append(taskButton1);
     taskButtonsDiv.append(taskButton2);
+    taskButtonsDiv.append(checkBox);
 
     task.append(taskTextBox);
     task.append(taskButtonsDiv);
